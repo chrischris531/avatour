41c7d736512503ea4d7e2dc74ea0573ae08ddae5
Chris Myers <cm1220@ic.ac.uk>
2024-02-08 17:02:40 +0000

    initial push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Joystick } from "react-joystick-component";
+import axios from "axios";
 
 function App() {
+  const url = "http://146.169.185.195:5000";
+  const handleMove = (e) => {
+    let x = e.x;
+    let y = e.y;
+    let data = { content: x.toString() + ", " + y.toString() };
+    console.log(data);
+
+    axios
+      .post(url, data)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      styles={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+      }}
+    >
+      <p>Avatour Lite</p>
+      <Joystick
+        size={100}
+        width={100}
+        // sticky={true}
+        baseColor="red"
+        stickColor="blue"
+        move={handleMove}
+      />
     </div>
   );
 }
